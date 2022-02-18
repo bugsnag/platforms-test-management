92bc44f8fcd9ade967ce7ff51c8f9479950b5aaa
Alex Moinet <alex@bugsnag.com>
2022-02-18 15:07:37 +0000

    Use simple schema and setup docker-compose structure

--- lib/test_management_service/test_reporter/report_handler.rb
+++ lib/test_management_service/test_reporter/report_handler.rb
@@ -13,27 +13,15 @@ module TestManagementService
         Mongoid.configure do |config|
           config.clients.default = {
             hosts: [host],
-            database: database,
+            database: database
           }
           config.log_level = :warn
         end
       end
 
       def insert_report(report)
-        document = {}
-        document['configuration'] = Configuration.new(report['configuration'])
-        document['build'] = Build.new(report['build'])
-        document['session'] = report['session'].map do |session|
-          session['elements'].map! do |element|
-            element['steps'].map! { |step| Step.new(step) } unless element['steps'].nil?
-            element['before'].map! { |before| Before.new(before) } unless element['before'].nil?
-            element['after'].map! { |after| After.new(after) } unless element['after'].nil?
-            Element.new(element)
-          end
-          Session.new(session)
-        end
-        report = Report.new(document)
-        report.save
+        document = Report.new(report)
+        document.save
       end
     end
   end
--- lib/test_management_service/test_reporter/report_schema.rb
+++ lib/test_management_service/test_reporter/report_schema.rb
@@ -6,97 +6,97 @@ module TestManagementService
       include Mongoid::Document
       include Mongoid::Timestamps::Created
       store_in collection: 'reports'
-      has_one :configuration, class_name: 'Configuration', inverse_of: :report
-      has_one :build, class_name: 'Build', inverse_of: :report
-      has_many :session, class_name: 'Session', inverse_of: :report
+      field :configuration, type: Hash
+      field :build, type: Hash
+      field :session, type: Array
 
-      accepts_nested_attributes_for :configuration, :build, :session
+      # accepts_nested_attributes_for :configuration, :build, :session
     end
 
-    class Configuration
-      include Mongoid::Document
+  #   class Configuration
+  #     include Mongoid::Document
 
-      belongs_to :report, class_name: 'Report', inverse_of: :configuration
-      field :driver_class, type: String
-      field :device_farm, type: String
-      field :device, type: String
-      field :os, type: String
-      field :os_version, type: String
-    end
+  #     belongs_to :report, class_name: 'Report', inverse_of: :configuration
+  #     field :driver_class, type: String
+  #     field :device_farm, type: String
+  #     field :device, type: String
+  #     field :os, type: String
+  #     field :os_version, type: String
+  #   end
 
-    class Build
-      include Mongoid::Document
+  #   class Build
+  #     include Mongoid::Document
 
-      belongs_to :report, class_name: 'Report', inverse_of: :build
-      field :pipeline, type: String
-      field :repo, type: String
-      field :build_url, type: String
-      field :branch, type: String
-      field :message, type: String
-      field :step, type: String
-      field :commit, type: String
-    end
+  #     belongs_to :report, class_name: 'Report', inverse_of: :build
+  #     field :pipeline, type: String
+  #     field :repo, type: String
+  #     field :build_url, type: String
+  #     field :branch, type: String
+  #     field :message, type: String
+  #     field :step, type: String
+  #     field :commit, type: String
+  #   end
 
-    class Session
-      include Mongoid::Document
+  #   class Session
+  #     include Mongoid::Document
 
-      belongs_to :report, class_name: 'Report', inverse_of: :session
-      unalias_attribute :id
-      field :id, type: String
-      field :uri, type: String
-      field :keyword, type: String
-      field :name, type: String
-      field :description, type: String
-      field :line, type: Integer
-      has_many :elements, class_name: 'Element', inverse_of: :session
+  #     belongs_to :report, class_name: 'Report', inverse_of: :session
+  #     unalias_attribute :id
+  #     field :id, type: String
+  #     field :uri, type: String
+  #     field :keyword, type: String
+  #     field :name, type: String
+  #     field :description, type: String
+  #     field :line, type: Integer
+  #     has_many :elements, class_name: 'Element', inverse_of: :session
 
-      accepts_nested_attributes_for :elements
-    end
+  #     accepts_nested_attributes_for :elements
+  #   end
 
-    class Element
-      include Mongoid::Document
+  #   class Element
+  #     include Mongoid::Document
 
-      belongs_to :session, class_name: 'Session', inverse_of: :element
-      unalias_attribute :id
-      field :id, type: String
-      field :keyword, type: String
-      field :name, type: String
-      field :description, type: String
-      field :line, type: Integer
-      field :type, type: String
-      has_many :steps, class_name: 'Step', inverse_of: :element
-      has_many :before, class_name: 'Before', inverse_of: :element
-      has_many :after, class_name: 'After', inverse_of: :element
+  #     belongs_to :session, class_name: 'Session', inverse_of: :element
+  #     unalias_attribute :id
+  #     field :id, type: String
+  #     field :keyword, type: String
+  #     field :name, type: String
+  #     field :description, type: String
+  #     field :line, type: Integer
+  #     field :type, type: String
+  #     has_many :steps, class_name: 'Step', inverse_of: :element
+  #     has_many :before, class_name: 'Before', inverse_of: :element
+  #     has_many :after, class_name: 'After', inverse_of: :element
 
-      accepts_nested_attributes_for :steps, :before, :after
-    end
+  #     accepts_nested_attributes_for :steps, :before, :after
+  #   end
 
-    class Step
-      include Mongoid::Document
+  #   class Step
+  #     include Mongoid::Document
 
-      belongs_to :element, class_name: 'Element', inverse_of: :steps
-      field :keyword, type: String
-      field :name, type: String
-      field :line, type: Integer
-      field :rows, type: Array
-      field :match, type: Hash
-      field :result, type: Hash
-    end
+  #     belongs_to :element, class_name: 'Element', inverse_of: :steps
+  #     field :keyword, type: String
+  #     field :name, type: String
+  #     field :line, type: Integer
+  #     field :rows, type: Array
+  #     field :match, type: Hash
+  #     field :result, type: Hash
+  #   end
 
-    class Before
-      include Mongoid::Document
+  #   class Before
+  #     include Mongoid::Document
 
-      belongs_to :element, class_name: 'Element', inverse_of: :before
-      field :match, type: Hash
-      field :result, type: Hash
-    end
+  #     belongs_to :element, class_name: 'Element', inverse_of: :before
+  #     field :match, type: Hash
+  #     field :result, type: Hash
+  #   end
 
-    class After
-      include Mongoid::Document
+  #   class After
+  #     include Mongoid::Document
 
-      belongs_to :element, class_name: 'Element', inverse_of: :after
-      field :match, type: Hash
-      field :result, type: Hash
-    end
+  #     belongs_to :element, class_name: 'Element', inverse_of: :after
+  #     field :match, type: Hash
+  #     field :result, type: Hash
+  #   end
   end
 end
